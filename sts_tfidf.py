8ab7e12e16e74af7d0c9ada2b539ac9b8b18b1ba
eliserust <eliserust@gmail.com>
2022-02-17 11:15:30 -0500

    TFIDF lab commit

--- sts_tfidf.py
+++ sts_tfidf.py
@@ -9,13 +9,28 @@ from nltk.corpus import stopwords
 import string
 from util import parse_sts
 import argparse
+import sys
+import warnings
+
+warnings.filterwarnings("ignore")
 
 
 def preprocess_text(text):
-    """Preprocess one sentence: tokenizes, lowercases, applies the Porter stemmer,
+    """Preprocess one sentence:
+    tokenizes, lowercases, applies the Porter stemmer,
      removes punctuation tokens and stopwords.
-     Returns a string of tokens joined by whitespace."""
-    return " ".join(text)
+     Returns a string of tokens joined by whitespace.
+     """
+
+    # Stemmer
+    ps = PorterStemmer()
+    text_clean = ps.stem(text)
+    # tokenize + lowercase
+    text_clean = word_tokenize(text.clean.lower())
+    # remove punctuation
+    # removes stopwords
+
+    return " ".join(text_clean)
 
 
 def main(sts_data):
@@ -23,16 +38,31 @@ def main(sts_data):
     texts, labels = parse_sts(sts_data)
 
     # TODO 1: get a single list of texts to determine vocabulary and document frequency
+
+    # get a single list of texts to determine vocabulary and document frequency
+    all_t1, all_t2 = zip(*texts)
+    print(f"{len(all_t1)} texts total")
+    all_texts = all_t1 + all_t2
+
     # create a TfidfVectorizer
+    vectorizer = TfidfVectorizer(input = "content", lowercase = True, analyzer = "word", use_idf = True, min_df = 10)
+
     # fit to the training data
+    tfidf_vector = vectorizer.fit(all_texts)
+    print(tfidf_vector.get_feature_names())
 
 
     # TODO 2: Can normalization like removing stopwords remove differences that aren't meaningful?
     # define the function preprocess_text above
     preproc_train_texts = [preprocess_text(text) for text in texts]
+    print(preproc_train_texts)
 
     # TODO 3: Learn another TfidfVectorizer for preprocessed data
     # Use token_pattern "\S+" in the TfidfVectorizer to split on spaces
+    vectorizer_preproc = TfidfVectorizer(input = "content", lowercase = True, analyzer = "word", use_idf = True, min_df = 10,
+                                         token_pattern = "\S+")
+    tfidf_preproc = vectorizer_preproc.fit(preproc_train_texts)
+    print(tfidf_preproc.get_feature_names())
 
     # TODO 4: compute cosine similarity for each pair of sentences, both with and without preprocessing
     cos_sims = []
